5f1229f34ef7ae5afbfce25daf0c9caca180fe3f
C0atRack <brennanromero2022@gmail.com>
2024-04-03 03:43:41 +0000

    Fixed the equipment model so Django could migrate it.

--- equipment_app/models.py
+++ equipment_app/models.py
@@ -6,28 +6,28 @@ class Employee(models.Model):
 
 class EquipmentModel(models.Model):
 
-    EQUIPMENT_CATEGORIES = {
-        'Accessory' : "Assessory",
-        'Test Equipment' : 'Test Equipment',
-        'Consumable' : 'Consumable',
-    }
+    EQUIPMENT_CATEGORIES = (
+        ('Accessory', "Assessory"),
+        ('Test Equipment', 'Test Equipment'),
+        ('Consumable', 'Consumable'),
+    )
 
-    SerialNumber = models.CharField(max_length=200, null=False)
-    ModelNumber = models.CharField(max_length=200, null=False)
-    AssetTag = models.CharField(max_length=200, null=False)
-    Category = models.CharField(choices=EQUIPMENT_CATEGORIES)
+    SerialNumber = models.CharField(max_length=200, blank=False)
+    ModelNumber = models.CharField(max_length=200, blank=False)
+    AssetTag = models.CharField(max_length=200, blank=False)
+    Category = models.CharField(max_length=100, choices=EQUIPMENT_CATEGORIES)
     Img = models.ImageField(upload_to="uploads/")
-    Description = models.TextField(max_length=5000, null=False)
+    Description = models.TextField(max_length=5000, blank=False)
     ManualLink = models.URLField(max_length=1000)
 
     CalDueDate = models.DateField()
     WaranteeExpires = models.DateField()
 
-    CheckedOutTo = models.ForeignKey(Employee, null=True, on_delete=models.DO_NOTHING)
+    CheckedOutTo = models.ForeignKey(Employee, null=True, on_delete=models.SET_NULL)
     CheckOutDate = models.DateField()
     CheckOutLocation = models.CharField(max_length=200)
 
-    AccessoryOf = models.ForeignKey('self', on_delete=models.SET_NULL)
+    AccessoryOf = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
         
 
 
