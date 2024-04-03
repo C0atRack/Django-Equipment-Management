d71d132a7fe5904811eef6ba855f0372fb2dc7b5
C0atRack <brennanromero2022@gmail.com>
2024-04-03 05:27:09 +0000

    Added index view in prep of adding the index page

--- equipment_app/views.py
+++ equipment_app/views.py
@@ -3,3 +3,6 @@ from django.views.generic import *
 # Create your views here.
 class Stub(TemplateView):
     template_name = "equipment_app/stub.html"
+
+class Index(TemplateView):
+    template_name = "equipment_app/stub.html"
