d50d9aaec0cabed38b020fe265c27b56056fb1e4
C0atRack <brennanromero2022@gmail.com>
2024-04-03 03:37:46 +0000

    Not yet working, but almost done adding Equipment Model

--- equipment_app/models.py
+++ equipment_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    pass
+
+
+class EquipmentModel(models.Model):
+
+    EQUIPMENT_CATEGORIES = {
+        'Accessory' : "Assessory",
+        'Test Equipment' : 'Test Equipment',
+        'Consumable' : 'Consumable',
+    }
+
+    SerialNumber = models.CharField(max_length=200, null=False)
+    ModelNumber = models.CharField(max_length=200, null=False)
+    AssetTag = models.CharField(max_length=200, null=False)
+    Category = models.CharField(choices=EQUIPMENT_CATEGORIES)
+    Img = models.ImageField(upload_to="uploads/")
+    Description = models.TextField(max_length=5000, null=False)
+    ManualLink = models.URLField(max_length=1000)
+
+    CalDueDate = models.DateField()
+    WaranteeExpires = models.DateField()
+
+    CheckedOutTo = models.ForeignKey(Employee, null=True, on_delete=models.DO_NOTHING)
+    CheckOutDate = models.DateField()
+    CheckOutLocation = models.CharField(max_length=200)
+
+    AccessoryOf = models.ForeignKey('self', on_delete=models.SET_NULL)
+        
+
+
--- equipment_manager/settings.py
+++ equipment_manager/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'equipment_manager'
+    'equipment_app'
 ]
 
 MIDDLEWARE = [
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
