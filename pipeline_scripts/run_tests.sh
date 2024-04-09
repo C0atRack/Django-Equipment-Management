7079d32027290db49541fd440753c58d315a32f9
C0atRack <brennanromero2022@gmail.com>
2024-04-09 23:04:26 +0000

    Fixing false positives on commits

--- pipeline_scripts/run_tests.sh
+++ pipeline_scripts/run_tests.sh
@@ -2,5 +2,4 @@
 mv "$(echo $ENVFILE | sed s/\\\'//g)" .
 source djvenv/bin/activate;
 ./manage.py makemigrations && ./manage.py migrate
-./manage.py test --verbosity 2
-rm .env
+exec ./manage.py test --verbosity 2
