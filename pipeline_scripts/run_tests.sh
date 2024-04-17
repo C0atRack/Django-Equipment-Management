1b496fbb9c1e86261ca3c3397c7ba0d48c4c23c4
C0atRack <brennanromero2022@gmail.com>
2024-04-17 04:19:28 +0000

    Forgot to add this to run coverage tests

--- pipeline_scripts/run_tests.sh
+++ pipeline_scripts/run_tests.sh
@@ -2,4 +2,4 @@
 mv "$(echo $ENVFILE | sed s/\\\'//g)" .
 source djvenv/bin/activate;
 ./manage.py makemigrations && ./manage.py migrate
-exec ./manage.py test --verbosity 2
+exec coverage run ./manage.py test --verbosity 2
