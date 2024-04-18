6463b94f060b4575182728f45e2d49a67eee1f20
C0atRack <brennanromero2022@gmail.com>
2024-04-18 17:24:44 +0000

    Using BytesIO and PIL I got the test working

--- equipment_app/tests/test_forms.py
+++ equipment_app/tests/test_forms.py
@@ -1,5 +1,7 @@
 import PIL.Image
+from io import BytesIO
 from django.test import TransactionTestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 from equipment_app.forms import EquipmentForm
 
 from datetime import datetime
@@ -11,6 +13,9 @@ class EquipmentFormTest(TransactionTestCase):
     serialized_rollback = True
 
     def test_form_verify_valid(self):
+        Img = PIL.Image.new("RGB", (100,100))
+        ImgOut = BytesIO()
+        Img.save(ImgOut, "PNG")
         date = datetime.today()
         data : dict = {
             'Name': "Test", 
@@ -24,6 +29,7 @@ class EquipmentFormTest(TransactionTestCase):
             'CalDueDate' : date,
             'WaranteeExpires' : date,
         }
-        testform = EquipmentForm(data=data)
+        uploadfiles = {'Img' : SimpleUploadedFile("TestImg.png", ImgOut.getvalue())}
+        testform = EquipmentForm(data=data, files=uploadfiles)
         print(testform.errors)
         self.assertTrue(testform.is_valid())
