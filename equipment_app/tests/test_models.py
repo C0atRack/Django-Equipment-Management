094ac48578e8897fca143027753a73fb8e8dcc23
C0atRack <brennanromero2022@gmail.com>
2024-04-23 04:25:16 +0000

    Lumb the urls tests together in the test_models.py

--- equipment_app/tests/test_models.py
+++ equipment_app/tests/test_models.py
@@ -11,10 +11,12 @@ class TestEquipmentUrls(TestCase):
         cls.equipment = EquipmentModel.objects.create(Name="Test" , SerialNumber=1234 , ModelNumber=1234 , AssetTag=1234 , Category="Test Equipment" , Img="uploads/test_equipment.png" , Description="Test Equipment", ManualLink="http://example.com" , CheckInLocation="Test Check in location" , CalDueDate=datetime.today(), WaranteeExpires=datetime.today() , CheckedOutTo=None , CheckOutDate=None, CheckOutLocation=None , AccessoryOf=None        )
         return super().setUpClass()
     
-    def test_CRUD_urls(self):
+    def test_urls(self):
         self.assertEqual(self.equipment.get_absolute_url(), f"/equipment/{self.equipment.id}")
         self.assertEqual(self.equipment.get_edit_absolute_url(), f"/equipment/edit/{self.equipment.id}")
         self.assertEqual(self.equipment.get_delete_absolute_url(), f"/equipment/delete/{self.equipment.id}")
+        self.assertEqual(self.equipment.get_checkout_url(), f"/equipment/checkout/{self.equipment.id}")
+        self.assertEqual(self.equipment.get_checkin_url(), f"/equipment/checkin/{self.equipment.id}")
     
     def test_availibility_check(self):
         emp = Employee()
@@ -23,9 +25,7 @@ class TestEquipmentUrls(TestCase):
         self.equipment.CheckedOutTo = None
         self.assertTrue(self.equipment.is_availible())
     
-    def test_checkX_urls(self):
-        self.assertEqual(self.equipment.get_checkout_url(), f"/equipment/checkout/{self.equipment.id}")
-        self.assertEqual(self.equipment.get_checkin_url(), f"/equipment/checkin/{self.equipment.id}")
+
 
 class TestEmployeeUrls(TestCase):
     @classmethod
