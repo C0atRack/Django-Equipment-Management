c83b485dc792e8b25f4f824a189be94252ac650b
C0atRack <brennanromero2022@gmail.com>
2024-04-25 23:37:56 +0000

    Fix testing being run in the inpromper env (Buffered stdout, bytecode file generation enabled)

--- pipeline_scripts/run_desktop_tests.sh
+++ pipeline_scripts/run_desktop_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source djvenv/bin/activate;
 ./manage.py makemigrations && ./manage.py migrate
-exec coverage run ./manage.py test --verbosity 2
+exec env coverage run ./manage.py test --verbosity 2
--- pipeline_scripts/run_mobile_tests.sh
+++ pipeline_scripts/run_mobile_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source djvenv/bin/activate;
 ./manage.py makemigrations && ./manage.py migrate
-BROWSER_WIDTH=414 BROWSER_HEIGHT=896 exec ./manage.py test --verbosity 2 equipment_app.tests.test_integrations
+BROWSER_WIDTH=414 BROWSER_HEIGHT=896 exec env ./manage.py test --verbosity 2 equipment_app.tests.test_integrations
